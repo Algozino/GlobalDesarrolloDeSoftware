ea46296bbc9232cff746134745a307f4485eddf5
Algozino <algozinojuan@gmail.com>
2025-11-17 00:04:35 -0300

    Delete src/main/java/org/example directory

--- src/main/java/org/example/exception/DnaHashCalculationException.java
+++
@@ -1,8 +0,0 @@
-package org.example.exception;
-
-// excepcion personalizada que pide la guía
-public class DnaHashCalculationException extends RuntimeException {
-    public DnaHashCalculationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
